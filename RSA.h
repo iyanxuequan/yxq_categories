02a469c5f94c8d9aae0c3e428f1d5a4f07f98700
iyanxuequan <iyan@163.com>
2016-06-29 10:42:51 +0800

    Create RSA.h

---
+++ RSA.h
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+
+@interface RSA : NSObject
+
++ (NSString *)encryptString:(NSString *)str publicKey:(NSString *)pubKey;
++ (NSString *)encryptData:(NSData *)data publicKey:(NSString *)pubKey;
+
+@end
